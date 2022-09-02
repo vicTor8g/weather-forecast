1a5a140dd844c0c4534942d42d12ca71207121cb
Victor Vasconcelos <developervasconcelos@gmail.com>
2022-09-02 16:47:52 +0100

    Update datetime.js

--- src/javascript/datetime.js
+++ src/javascript/datetime.js
@@ -4,3 +4,14 @@ const newDate = new Date();
 
 const currentHour = newDate.getHours();
 const currentMinute = newDate.getMinutes();
+
+const currentWeekDay = newDate.getDay();
+const currentMonth = newDate.getMonth();
+const currentMonthDay = newDate.getDate();
+const currentYear = newDate.getFullYear();
+
+const allWeekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const allMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+textTimeString.innerHTML = `
+    ${currentHour}:${currentMinute} - ${allWeekDays[currentWeekDay]}, ${currentMonthDay} ${allMonths[currentMonth]} ${currentYear}'`;
