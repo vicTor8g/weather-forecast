9e79c0c31f8406b33ed29c721214577312ee24f0
Victor Vasconcelos <developervasconcelos@gmail.com>
2022-09-02 16:37:40 +0100

    Create datetime.js

---
+++ src/javascript/datetime.js
@@ -0,0 +1,6 @@
+const textTimeString = document.querySelector('.content-text-time');
+
+const newDate = new Date();
+
+const currentHour = newDate.getHours();
+const currentMinute = newDate.getMinutes();
