ac370dd3a6c24d8171723500b79071567e8cb022
Victor Vasconcelos <developervasconcelos@gmail.com>
2022-09-02 15:56:30 +0100

    Update main.js

--- src/javascript/api/main.js
+++ src/javascript/api/main.js
@@ -3,16 +3,28 @@ const searchInput = document.querySelector('.section-extra-input');
 const searchButton = document.querySelector('.section-extra-button');
 
 const weatherLogic = () => {
-    const getGeocodingData = async () => {
+
+    const getCityCoordinates = async () => {
         const inputValue = searchInput.value;
         const geocodingURL = `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=1&appid=${apiKey}`;
 
         const geocodingFetchResponse = await fetch(geocodingURL);
         const geocodingJsonData = await geocodingFetchResponse.json();
 
-        console.log(geocodingJsonData);
+        let latitudeValue = geocodingJsonData[0].lat;
+        let longitudeValue = geocodingJsonData[0].lon;
+
+        const getCityWeather = async () => {
+            const openWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitudeValue}&lon=${longitudeValue}&appid=${apiKey}`;
+
+            const openWeatherFetchResponse = await fetch(openWeatherURL);
+            const openWeatherJsonData = await openWeatherFetchResponse.json();
+
+            console.log(openWeatherJsonData);
+        }
+        getCityWeather();
     }
-    getGeocodingData();
+    getCityCoordinates();
 }
 
 searchButton.addEventListener('click', weatherLogic);
